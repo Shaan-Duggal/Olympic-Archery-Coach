d30f0643535968d81b39b61eb6b1d816ee7d5c94
Shaan-Duggal <shaanduggal10@icloud.com>
2025-05-05 16:10:59 -0700

    Added Hello Archer! Welcome Screen

--- Olympic Archery Coach/ContentView.swift
+++ Olympic Archery Coach/ContentView.swift
@@ -9,12 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+      Text("👋 Hello, Archer!")
+        .font(.largeTitle)
         .padding()
     }
 }
