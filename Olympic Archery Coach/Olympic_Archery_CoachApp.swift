de8e5e3bdfb4454a50c994ab38d9ed117f1f9218
Shaan-Duggal <shaanduggal10@icloud.com>
2025-05-05 15:25:59 -0700

    Initial Commit

---
+++ Olympic Archery Coach/Olympic_Archery_CoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  Olympic_Archery_CoachApp.swift
+//  Olympic Archery Coach
+//
+//  Created by Shaan Duggal on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Olympic_Archery_CoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Olympic Archery CoachTests/Olympic_Archery_CoachTests.swift
@@ -0,0 +1,17 @@
+//
+//  Olympic_Archery_CoachTests.swift
+//  Olympic Archery CoachTests
+//
+//  Created by Shaan Duggal on 5/5/25.
+//
+
+import Testing
+@testable import Olympic_Archery_Coach
+
+struct Olympic_Archery_CoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
